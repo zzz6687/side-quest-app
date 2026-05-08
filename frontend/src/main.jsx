b28799db93fcd2bbd381ac0c2f2831263b038552
zzz6687 <agirova94@gmail.com>
2026-05-07 22:32:28 -0700

    removing the env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client.js";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
