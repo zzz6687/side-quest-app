3904dfd3ad927a0401a21ffa84fc2105ac060115
zzz6687 <agirova94@gmail.com>
2026-05-25 23:30:55 -0700

    bug

--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -92,7 +92,7 @@ const Admin = () => {
   return (
     <div>
       <div className={styles.container}>
-        {submissions.map(
+        {submissions.reverse().map(
           (item, index) =>
             item.quest && (
               <div className={styles.card} key={index}>
